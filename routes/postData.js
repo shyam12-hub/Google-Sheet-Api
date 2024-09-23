a410444dda566cfe2b361b95ff6157463538018d
shyam261510 <shyam261510@gmail.com>
2024-09-23 05:02:19 +0000

    changes in post data

--- routes/postData.js
+++ routes/postData.js
@@ -1,21 +1,26 @@
 import { Router } from "express";
-
 import { database } from "../fireBase/database.js";
-import { set,ref } from "firebase/database";
+import { set, ref } from "firebase/database";
+
 const router = Router();
 
-router.post("/",async(req,res)=>{
-try{
-    
-const {data} = req.body
-set(ref(database,"sheet-data"),{
-    data
-})
-return res.json({message:"sheet data send to data base ",data})
-}catch(e){
-    console.log("Could able to POST data");
-    return res.json({message:'Could not able to send data',error:e.message})
-}
-})
+router.post("/", async (req, res) => {
+    try {
+        const { data } = req.body;
 
-export default router
+        // Validate incoming data
+        if (!data || !data.message) {
+            return res.status(400).json({ message: 'Invalid data format. Expecting { data: { message: "..." } }' });
+        }
+
+        // Save data to Firebase
+        await set(ref(database, "sheet-data"), { data });
+
+        return res.status(201).json({ message: "Sheet data sent to database", data });
+    } catch (e) {
+        console.error("Could not POST data:", e);
+        return res.status(500).json({ message: 'Could not send data', error: e.message });
+    }
+});
+
+export default router;
